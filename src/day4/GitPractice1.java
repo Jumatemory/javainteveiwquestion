13abce3d284e97e779179d54a1c7d66bb843e0b3
Jumatemory <*Case#255>
2020-07-21 20:42:15 -0400

    commit

---
+++ src/day4/GitPractice1.java
@@ -0,0 +1,10 @@
+package day4;
+
+public class GitPractice1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("hello world");
+        }
+
+    }
+}
